aaf68835e1ff4d019ce2a3c5f041f84f8f806648
jason <981909006@qq.com>
2022-05-26 09:46:33 +0800

    fix:updata

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -27,8 +27,8 @@ const Landing = () => {
       id="home"
       ref={ref}
       sx={{
-        height: "100vh",
         paddingTop: "68px",
+        paddingBottom:{xs:'60px',md:''},
         background:
           "url(/images/landingBg.png), linear-gradient(90deg, #1F213B 0%, #1E1F3A 100%)",
         backgroundPosition: "bottom, center",
@@ -36,97 +36,100 @@ const Landing = () => {
         backgroundSize: "100%, cover",
       }}
     >
-       <Container
+      <ImageContainer
+          url="/images/landing-right-bubbles.png"
+          sx={{ maxWidth: { xs: '250px', md: '37vw' }, height: 'fit-content' , display:{xs:'block',md:'none'},margin:{xs:'0 auto'} }}
+        />
+      <Container
         sx={{
-          paddingBottom: { xs: '60px', md: '160px' },
+          paddingBottom: { xs: "60px", md: "160px" },
         }}
         ref={ref}
       >
-      <Box
-        sx={{ flexDirection: { xs: "column", md: "row" } }}
-        paddingBottom={{
-          xs: "40px",
-        }}
-      >
         <Box
-          sx={{
-            flex: "0 0 50%",
-            paddingLeft: { md: "70px" },
-            margin: "10% auto 0 auto"
+          sx={{ flexDirection: { xs: "column", md: "row" } }}
+          paddingBottom={{
+            xs: "40px",
           }}
         >
-          <Typography
-            textAlign="center"
-            color="text.primary"
-            fontSize={{ xs: "26px", md: "30px" }}
-            fontWeight="900"
-            lineHeight={{ md: "63px" }}
+          <Box
+            sx={{
+              flex: "0 0 50%",
+              paddingLeft: { md: "70px" },
+              margin: "10% auto 0 auto",
+            }}
           >
-            Fantasy Games is building a world-wide gaming platform that provides
-            a fun and fair skill-based mobile gaming experience to everyone.
-          </Typography>
+            <Typography
+              textAlign="center"
+              color="text.primary"
+              fontSize={{ xs: "26px", md: "30px" }}
+              fontWeight="900"
+              lineHeight={{ md: "63px" }}
+            >
+              Fantasy Games is building a world-wide gaming platform that
+              provides a fun and fair skill-based mobile gaming experience to
+              everyone.
+            </Typography>
+          </Box>
         </Box>
-      </Box>
       </Container>
 
       <Container
         maxWidth="lg"
         sx={{
-          display: {xs:'',md:'flex'},
+          display: { xs: "", md: "flex" },
           flexDirection: { xs: "column", md: "row" },
           alignItems: "top",
-          justifyContent:  "space-between",
-
+          justifyContent: "space-between",
         }}
       >
-      
-          <Box
-            mb={{ xs: 2, md: "45px" }}
-            sx={{
-              width: "100%",
-              marginRight:{md:'50px'}
-              // background: 'url(/images/landing-right-bubbles.png) no-repeat right center  / contain',
-            }}
-          >
-            <Box>
+        <Box
+          mb={{ xs: 2, md: "45px" }}
+          sx={{
+            width: "100%",
+            marginRight: { md: "50px" },
+            // background: 'url(/images/landing-right-bubbles.png) no-repeat right center  / contain',
+          }}
+        >
+          <Box>
+            <Typography
+              color="text.primary"
+              fontSize={{ xs: "22px", md: "30px" }}
+              fontWeight="900"
+              lineHeight={{ md: "63px" }}
+              mb={{ xs: 2, md: "50px" }}
+              whiteSpace="nowrap"
+            >
+              An Esports gaming platform
+            </Typography>
+            <Typography
+              component="ul"
+              sx={{
+                listStylePosition: "outside",
+                paddingLeft: "24px",
+              }}
+            >
               <Typography
-                color="text.primary"
-                fontSize={{ xs: "22px", md: "30px" }}
-                fontWeight="900"
-                lineHeight={{ md: "63px" }}
-                mb={{ xs: 2, md: "50px" }}
-                whiteSpace="nowrap"
+                component="li"
+                fontSize={{ xs: "14px", md: "24px" }}
+                lineHeight={{ md: "44px" }}
+                fontWeight="700"
               >
-                An Esports gaming platform
+                Tons of great games to decorate your life !
               </Typography>
               <Typography
-                component="ul"
-                sx={{
-                  listStylePosition: "outside",
-                  paddingLeft: "24px",
-                }}
+                component="li"
+                fontSize={{ xs: "14px", md: "24px" }}
+                lineHeight={{ md: "44px" }}
+                fontWeight="700"
               >
-                <Typography
-                  component="li"
-                  fontSize={{ xs: "14px", md: "24px" }}
-                  lineHeight={{ md: "44px" }}
-                  fontWeight="700"
-                >
-                  Tons of great games to decorate your life !
-                </Typography>
-                <Typography
-                  component="li"
-                  fontSize={{ xs: "14px", md: "24px" }}
-                  lineHeight={{ md: "44px" }}
-                  fontWeight="700"
-                >
-                  It's more fun and relaxing than the games your ever played!
-                  Conquer the real players of your skill level in fair and fun
-                  games.
-                </Typography>
+                It's more fun and relaxing than the games your ever played!
+                Conquer the real players of your skill level in fair and fun
+                games.
               </Typography>
-            </Box>
+            </Typography>
           </Box>
+        </Box>
         {/* <ImageContainer
           url="/images/landing-right-bubbles.png"
           sx={{ maxWidth: { xs: '250px', md: '37vw' }, height: 'fit-content' }}
@@ -135,7 +138,7 @@ const Landing = () => {
           mb={{ xs: 2, md: "45px" }}
           sx={{
             width: "80%",
-            marginLeft:{md:'50px'}
+            marginLeft: { md: "50px" },
             // background: 'url(/images/landing-right-bubbles.png) no-repeat right center  / contain',
           }}
         >
@@ -198,7 +201,7 @@ const Landing = () => {
             borderRadius: "14px",
             fontSize: { md: "24px" },
             cursor: Links.app ? "pointer" : "not-allowed",
-            margin: "0 auto"
+            margin: "0 auto",
           }}
         >
           {Links.app ? "Get App" : "Coming Soon"}
